0965f3beccd05b1b36707452da22da2e46a75faa
Tat Vi Quyen <tvq9612@gmail.com>
2020-01-24 09:38:28 +0700

    add link tvqhub

--- packages/deep/resources/views/master.blade.php
+++ packages/deep/resources/views/master.blade.php
@@ -37,6 +37,7 @@
         <p>{{ config('deep.description') }}</p>
         <ul class="menu">
             <li><a href="{{ route('deep.indexVideo') }}">#video</a></li>
+            <li><a href="https://tvqhub.com">#tvqhub</a></li>
         </ul>
         <p></p>
     </div>
