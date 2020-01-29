2911eb6e78a930231b9e5364763b9d830981396f
Tat Vi Quyen <tvq9612@gmail.com>
2020-01-29 12:23:45 +0700

    redirect route

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group(['middleware' => 'auth'], function() {
     Route::group(['prefix' => 'ama', 'as' => 'ama.'], function() {
         Route::get('/', 'Admin\AmaQuestionController@index');
         Route::get('{id}', 'Admin\AmaQuestionController@show')->name('detail');
-        Route::match(['put', 'post'], '{id}', 'Admin\AmaQuestionController@update')->name('update');
+        Route::put('{id}', 'Admin\AmaQuestionController@update')->name('update');
         Route::delete('{id}', 'Admin\AmaQuestionController@destroy')->name('destroy');
         Route::delete('force/{id}', 'Admin\AmaQuestionController@forceDelete')->name('forceDelete');
         Route::patch('restore/{id}', 'Admin\AmaQuestionController@restore')->name('restore');
