2af80d0a40a9b7beaa62151a98abde2cf3ec749f
Tat Vi Quyen <tvq9612@gmail.com>
2020-01-29 10:09:11 +0700

    Create share.blade.php

---
+++ resources/views/ama/share.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $question->question }}</title>
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="{{ $question->question }}"/>
+    <meta property="og:description" content="{{ strip_tags($question->answer) }}"/>
+    <meta property="og:image" content="{{ asset('images/ama-tvq-400.png') }}"/>
+</head>
+
+<body>
+<script>
+    location.href = '{{ config('app.master_url') }}/ama';
+</script>
+</body>
+</html>
