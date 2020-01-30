99e21d8d3e0af26c09f906445a73ee958af51bd4
Tat Vi Quyen <tvq9612@gmail.com>
2020-01-30 19:53:40 +0700

    fix space

--- app/Http/Controllers/Admin/AmaQuestionController.php
+++ app/Http/Controllers/Admin/AmaQuestionController.php
@@ -18,7 +18,8 @@ class AmaQuestionController extends Controller
      * AmaQuestionController constructor.
      * @param AmaQuestionRepositoryInterface $repository
      */
-    public function __construct(AmaQuestionRepositoryInterface $repository) {
+    public function __construct(AmaQuestionRepositoryInterface $repository)
+    {
         $this->repository = $repository;
     }
 
