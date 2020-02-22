86bc6e1620b1859d9b0e7fbd8f62f7e331d16d77
Tat Vi Quyen <tvq9612@gmail.com>
2020-02-22 10:57:31 +0700

    refactor routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 /**
  * Subdomain need to defined all above main domain.
  */
-
 // URL Shorten link
 Route::group(['domain' => 'ly' . config('session.domain'), 'as' => 'url.site.'], function() {
     Route::get('/', 'Site\UrlController@index')->name('index');
@@ -25,30 +24,30 @@ Route::group(['domain' => 'ly' . config('session.domain'), 'as' => 'url.site.'],
     });
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+/**
+ * Lar.tvqhub
+ */
 Auth::routes(['register' => false]);
 
+// No Auth needed
+Route::get('/', 'Controller@welcome');
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('share/{id}', 'Admin\AmaQuestionController@share')->name('ama.share'); // AMA share question to FB
 
-// AMA share question to FB
-Route::get('share/{id}', 'Admin\AmaQuestionController@share')->name('ama.share');
-
-Route::group(['middleware' => 'auth'], function() {
+// Admin (Auth needed)
+Route::group(['middleware' => 'auth', 'namespace' => 'Admin'], function() {
     Route::group(['prefix' => 'ama', 'as' => 'ama.'], function() {
-        Route::get('/', 'Admin\AmaQuestionController@index');
-        Route::get('{id}', 'Admin\AmaQuestionController@show')->name('show');
-        Route::put('{id}', 'Admin\AmaQuestionController@update')->name('update');
-        Route::delete('{id}', 'Admin\AmaQuestionController@destroy')->name('destroy');
-        Route::delete('force/{id}', 'Admin\AmaQuestionController@forceDelete')->name('forceDelete');
-        Route::patch('restore/{id}', 'Admin\AmaQuestionController@restore')->name('restore');
+        Route::get('/', 'AmaQuestionController@index');
+        Route::get('{id}', 'AmaQuestionController@show')->name('show');
+        Route::put('{id}', 'AmaQuestionController@update')->name('update');
+        Route::delete('{id}', 'AmaQuestionController@destroy')->name('destroy');
+        Route::delete('force/{id}', 'AmaQuestionController@forceDelete')->name('forceDelete');
+        Route::patch('restore/{id}', 'AmaQuestionController@restore')->name('restore');
     });
-    Route::resource('chinese-playlist', 'Admin\ChinesePlaylistController')->names([
+    Route::resource('chinese-playlist', 'ChinesePlaylistController')->names([
         'index' => 'chinese-playlist.'
     ]);
-    Route::resource('url', 'Admin\UrlController')->names([
+    Route::resource('url', 'UrlController')->names([
         'index' => 'url.'
     ]);
 });
