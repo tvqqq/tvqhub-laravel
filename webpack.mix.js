ed961942b7ed9ee1f2cd50259091a2c5bb501336
Tat Vi Quyen <tvq9612@gmail.com>
2020-04-12 08:15:56 +0700

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,14 +14,14 @@ const mix = require('laravel-mix');
 mix.disableNotifications();
 
 /* App */
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
 
 /* Package deep */
-mix.copyDirectory('packages/deep/resources/assets', 'public/packages/deep');
+// mix.copyDirectory('packages/deep/resources/assets', 'public/packages/deep');
 
 /* Url Shortlink (resources/views/url/site) */
-mix.js('resources/views/url/site/url.js', 'public/js');
+// mix.js('resources/views/url/site/url.js', 'public/js');
 
 /* Facebooker */
 mix.js('resources/views/facebooker/facebooker.js', 'public/js');
