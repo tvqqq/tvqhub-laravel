02827bedb856b4cb6c8f17fa58e945124b339ff9
Tat Vi Quyen <tvq9612@gmail.com>
2020-07-04 11:36:00 +0700

    integrate Vue as front end framework

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>@yield('title') :: {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
